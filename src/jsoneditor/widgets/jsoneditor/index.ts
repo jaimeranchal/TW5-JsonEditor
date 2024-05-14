5fb016f5dd4c8c651bf53a882a1a1dfc3bc5596f
Jaime Ranchal <jranchal@emeal.nttdata.com>
2024-05-14 12:16:54 +0200

    Error display for missing required widget parameters

--- src/jsoneditor/widgets/jsoneditor/index.ts
+++ src/jsoneditor/widgets/jsoneditor/index.ts
@@ -7,14 +7,94 @@ import { initEditor } from '../utils/initEditor';
 
 import { personSchema } from '../sampledata/schemas'; // only during testing
 
+const errorTypes = {
+  'widgetCall': 'Syntax error in widget call'
+}
+const messages = {
+  'schemaErr': 'Schema cannot be empty',
+  'saveModeErr': "SaveMode values can be 'json', 'tw5' or empty" 
+}
+
 class JsonEditorWidget extends Widget {
+  /* Attributes */
+  //---------------------------------------------------------------------------
   editor?: JSONEditor.JSONEditor<unknown>;
   containerElement?: HTMLDivElement;
   errorValidatorInfoElement ?: HTMLSpanElement;
-  currenTiddlerTitle?: string;
+  schema?: string;      // JSON schema as string
+  saveOptions?: { 
+    mode: string,
+    outputTiddler: string,
+    outputField: string
+  }
+  errorMessages?: { error: string, message: string}[];
 
-  public refresh(_changedTiddlers: IChangedTiddlers) {
+
+  /* Methods */
+  //---------------------------------------------------------------------------
+  private setAttributes(): void {
+    let schema = this.getAttribute('schema');
+    let saveMode = this.getAttribute('saveMode') ?? 'json';
+    let tiddler = this.getAttribute('tiddler');
+    let field = this.getAttribute('field');
+    let errors: typeof this.errorMessages = []
+
+    // check for errors
+    if (schema === undefined) {
+      errors.push({ 
+        'error': errorTypes.widgetCall, 
+        'message': messages.schemaErr 
+      });
+    }
+
+    switch (saveMode) {
+      case 'json':
+        let options = { 
+          mode: saveMode,
+          outputTiddler: tiddler ?? this.getVariable('currentTiddler'),
+          outputField: field ?? 'json-data'
+        }
+        this.saveOptions = options;
+        break;
+      case 'tw5':
+        break;
+      default:
+        errors.push({ 
+          'error': errorTypes.widgetCall, 
+          'message': messages.saveModeErr 
+        });
+        break;
+    }
+
+    if (errors.length > 0) this.errorMessages = errors;
+  }
+  private printErrors(container: Element): void {
+    let errorContainer = document.createElement('div');
+    this.errorMessages?.forEach(error => {
+      let errorSpan = document.createElement('span');
+      errorSpan.className = 'label label-warning';
+      errorSpan.textContent = `${error.error}: ${error.message}`;
+      errorContainer.appendChild(errorSpan);
+    });
+
+    container.appendChild(errorContainer);
+    this.domNodes.push(container);
+  }
+
+  public refresh(_changedTiddlers: IChangedTiddlers): boolean {
+    const changedAttributes = this.computeAttributes();
+    // Check if required widget parameters have been changed
+    if ($tw.utils.count(changedAttributes) > 0) {
+      this.refreshSelf();
+      return true;
+    }
+    // else do nothing
     return false;
+  }
+
+  public refreshSelf(): void {
+    if (this.editor === undefined) return;
+    // set values for fields being edited
   }
 
   public render(parent: Element, nextSibling: Element | null): void {
@@ -22,7 +102,12 @@ class JsonEditorWidget extends Widget {
     this.parentDomNode = parent;
     this.computeAttributes();
     this.execute();
-    this.currenTiddlerTitle = this.getAttribute('tiddler') ?? this.getVariable('currentTiddler');
+    this.setAttributes();
+
+    if (this.errorMessages !== undefined) {
+      this.printErrors(parent);
+      return
+    }
 
     if (this.editor === undefined) {
       // create container elements
@@ -41,14 +126,12 @@ class JsonEditorWidget extends Widget {
       parent.insertBefore(containerElement, nextSibling);
       this.domNodes.push(containerElement);
 
-      // Get json-editor attributes
+      // Get json-editor attributes and check for errors in widget call
       // Init json-editor
       this.editor = initEditor(JSON.parse(personSchema), editorElement, {})
     }
   }
 }
 
-declare let exports: {
-  RandomNumber: typeof JsonEditorWidget;
-};
-exports.RandomNumber = JsonEditorWidget;
+declare let exports: Record<string, typeof JsonEditorWidget>;
+exports['json-editor'] = JsonEditorWidget;
